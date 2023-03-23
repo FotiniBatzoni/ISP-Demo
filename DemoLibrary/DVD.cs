7d33d993387d9c8cc0053f1dc76d284035e45e57
fotini <f.batzoni@gmail.com>
2023-03-23 15:22:44 +0200

    Finishing with Interfaces

--- DemoLibrary/DVD.cs
+++ DemoLibrary/DVD.cs
@@ -8,12 +8,10 @@ namespace DemoLibrary
 {
     public class DVD : ILibraryItem
     {
-        public string Author { get; set; } = "";
         public DateTime BorrowDate { get; set; }
         public string Borrower { get; set; }
         public int CheckOutDurationInDays { get; set; } = 14;
         public string LibraryId { get; set; }
-        public int Pages { get; set; } = -1;
         public string Title { get; set; }
 
         public List<string> Actors { get; set; }
--- DemoLibrary/IBorrowableDVD.cs
+++ DemoLibrary/IBorrowableDVD.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public interface IBorrowableAudioBook : IAudioBook, IBorrowable
+    internal interface IBorrowableDVD
     {
     }
 }
